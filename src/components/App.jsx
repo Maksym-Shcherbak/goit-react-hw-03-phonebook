cb5368036f49b6ab566e1d8539fd7b0069ade430
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-10-27 20:41:32 +0300

    upd phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,28 @@ import { SearchFilter } from './SearchFilter/SearchFilter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
+    //   [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
 
   addContact = newContact => {
     const { name, number } = newContact;
@@ -46,9 +60,11 @@ export class App extends Component {
 
   getFilteredContacts = () => {
     const { contacts, filter } = this.state;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+    if (contacts) {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    }
   };
 
   isInPhonebook = name => {
@@ -60,19 +76,19 @@ export class App extends Component {
 
   render() {
     const filteredContacts = this.getFilteredContacts();
-    const { filter } = this.state;
+    const { contacts, filter } = this.state;
     return (
       <div className="container">
         <h1 className="phonebookTitle">Phonebook</h1>
         <ContactForm onAddContact={this.addContact}></ContactForm>
         <h2 className="contactsTitle">Contacts</h2>
-        {this.state.contacts.length !== 0 && (
+        {contacts && contacts.length !== 0 && (
           <Contactlist
             contacts={filteredContacts}
             onDeleteContact={this.deleteContact}
           ></Contactlist>
         )}
-        {this.state.contacts.length > 1 && (
+        {contacts && contacts.length > 1 && (
           <SearchFilter
             filter={filter}
             onHandleChange={this.searchContact}
